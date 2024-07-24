7d6eba16629c516149e12eeb0ab8951e922974ed
Zhao Siqi <89009573+13mn@users.noreply.github.com>
2024-07-24 10:04:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask,render_template,request
+import google.generativeai as palm
+import os
+
+api = os.getenv("AIzaSyCs9ymKydQBR5n17KpoAY2DVRKyNE7_doc")
+palm.configure(api_key=api)
+model = {"model":"models/chat-bison-001"}
 
 app = Flask(__name__)
 
@@ -9,7 +15,13 @@ def index():
 @app.route("/main",methods=["GET","POST"])
 def main():
     r = request.form.get("q")
-    return(render_template("main.html", r=r))
+    return(render_template("main.html",r=r))
+
+@app.route("/genAI",methods=["GET","POST"])
+def genAI():
+    q = request.form.get("q")
+    r = palm.chat(**model,messages=q)
+    return(render_template("genAI.html",r=r.last))
 
 if __name__ == "__main__":
-    app.run() 
+    app.run()
